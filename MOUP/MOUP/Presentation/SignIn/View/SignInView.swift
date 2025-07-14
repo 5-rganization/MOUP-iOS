3c0c4b6393f2a89f2932ed89e58d43696bdc6def
_GGDol <bohg6799@naver.com>
2025-07-14 23:30:01 +0900

    feat: 로그인 UI 구현
    Github issue #3

--- MOUP/MOUP/Presentation/SignIn/View/SignInView.swift
+++ MOUP/MOUP/Presentation/SignIn/View/SignInView.swift
@@ -5,4 +5,79 @@
 //  Created by 서동환 on 7/12/25.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+import Then
+
+final class SignInView: UIView {
+    // MARK: - Properties
+    
+    // MARK: - UI Components
+    private let logo = UIImageView().then {
+        $0.contentMode = .scaleAspectFit
+        $0.image = .logo
+        
+    }
+    
+    private(set) var appleLoginButton = UIButton().then {
+        $0.setImage(.appleSignInButton, for: .normal)
+    }
+    
+    private(set) var googleLoginButton = UIButton().then {
+        $0.setImage(.googleSignInButton, for: .normal)
+    }
+    
+    // MARK: - Initializer
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configure()
+    }
+    
+    @available(*, unavailable, message: "storyboard is not supported.")
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented.")
+    }
+    
+    // MARK: - Public Methods
+}
+
+private extension SignInView {
+    // MARK: - configure
+    func configure() {
+        setHierarchy()
+        setStyles()
+        setConstraints()
+    }
+    
+    // MARK: - setHierarchy
+    func setHierarchy() {
+        addSubviews(
+            logo,
+            appleLoginButton,
+            googleLoginButton
+        )
+    }
+    
+    // MARK: - setStyles
+    func setStyles() {
+        backgroundColor = .white
+    }
+    
+    // MARK: - setConstraints
+    func setConstraints() {
+        logo.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(208)
+            $0.horizontalEdges.equalToSuperview().inset(71.5)
+        }
+        appleLoginButton.snp.makeConstraints {
+            $0.top.equalTo(logo.snp.bottom).offset(100)
+            $0.horizontalEdges.equalToSuperview().inset(16)
+        }
+        googleLoginButton.snp.makeConstraints {
+            $0.top.equalTo(appleLoginButton.snp.bottom).offset(16)
+            $0.horizontalEdges.equalToSuperview().inset(16)
+        }
+    }
+}
+
