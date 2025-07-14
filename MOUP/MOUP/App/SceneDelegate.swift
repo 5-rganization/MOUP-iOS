9413039f358a662be88f02885059e5a5dcfe4198
_GGDol <bohg6799@naver.com>
2025-07-14 15:19:27 +0900

    feat: Coordinator 패턴 적용
    Github issue #1

--- MOUP/MOUP/App/SceneDelegate.swift
+++ MOUP/MOUP/App/SceneDelegate.swift
@@ -8,16 +8,19 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
-
+    var appCoordinator: Coordinator?
+    
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = TabBarViewController()
-        window.makeKeyAndVisible()
         self.window = window
+        
+        let appCoordinator = AppCoordinator(window: window)
+        self.appCoordinator = appCoordinator
+        appCoordinator.start()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
