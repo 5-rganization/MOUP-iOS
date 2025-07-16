6360897a6f3434ec27b9000a58471c0d10a11efa
SongKyuSeob <sks5075@naver.com>
2025-07-16 21:44:43 +0900

    feat: Home 내 최상단 바 UI까지 구현

--- MOUP/MOUP/Presentation/Home/View/HomeView.swift
+++ MOUP/MOUP/Presentation/Home/View/HomeView.swift
@@ -5,4 +5,84 @@
 //  Created by 서동환 on 7/12/25.
 //
 
-import Foundation
+import UIKit
+
+final class HomeView: UIView {
+    // MARK: - Properties
+
+    // MARK: - UI Components
+    private let topBar = UIView()
+
+    private let logoImageView = UIImageView().then {
+        $0.image = .homeAppTitle
+    }
+
+//    private let refreshButton = UIButton().then {
+//        $0.setImage(.refreshButton, for: .normal)
+//        $0.tintColor = .gray700
+//    }
+
+    private let refreshButton = UIButton().then {
+        var config = UIButton.Configuration.plain()
+        config.contentInsets = NSDirectionalEdgeInsets(top: 13.75, leading: 12.98, bottom: 13.75, trailing: 12.98)
+        config.image = UIImage.refreshButton
+        $0.configuration = config
+    }
+
+    // MARK: - Initializer
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        configure()
+    }
+
+    @available(*, unavailable, message: "storyboard is not supported.")
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented.")
+    }
+
+    // MARK: - Public Methods
+}
+
+private extension HomeView {
+    // MARK: - configure
+    func configure() {
+        setHierarchy()
+        setStyles()
+        setConstraints()
+    }
+
+    // MARK: - setHierarchy
+    func setHierarchy() {
+        addSubviews(topBar)
+        topBar.addSubviews(logoImageView, refreshButton)
+    }
+
+    // MARK: - setStyles
+    func setStyles() {
+        
+    }
+
+    // MARK: - setConstraints
+    func setConstraints() {
+        topBar.snp.makeConstraints {
+            $0.top.equalTo(safeAreaLayoutGuide)
+            $0.directionalHorizontalEdges.equalToSuperview()
+            $0.height.equalTo(50)
+        }
+
+        logoImageView.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(16)
+            $0.centerY.equalToSuperview()
+            $0.width.equalTo(75)
+            $0.height.equalTo(32)
+        }
+
+        refreshButton.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(8)
+            $0.centerY.equalToSuperview()
+            $0.size.equalTo(44)
+        }
+    }
+}
+
--- MOUP/MOUP/Presentation/Home/ViewController/HomeViewController.swift
+++ MOUP/MOUP/Presentation/Home/ViewController/HomeViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 final class HomeViewController: UIViewController {
     weak var coordinator: HomeCoordinator?
-    
+    private let homeViewModel: HomeViewModel
+    private let homeView = HomeView()
+
+
+    // MARK: - loadView
+    override func loadView() {
+        view = homeView
+    }
+
+    // MARK: - Initializer
+    init(coordinator: HomeCoordinator? = nil, homeViewModel: HomeViewModel) {
+        self.coordinator = coordinator
+        self.homeViewModel = homeViewModel
+
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented.")
+    }
+
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemRed
-        title = "홈"
+
+        configure()
     }
 }
 
+private extension HomeViewController {
+    func configure() {
+        setStyles()
+        setBindings()
+    }
+
+    func setStyles() {
+        self.navigationController?.navigationBar.isHidden = true
+    }
+
+    func setBindings() {
+
+    }
+}
