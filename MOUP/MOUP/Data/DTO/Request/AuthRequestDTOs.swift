4061aeb1ece33350d663a885c47abd4a43089b20
SongKyuSeob <sks5075@naver.com>
2025-08-12 14:09:36 +0900

    feat: 라우터 구조 변경을 위한 DTO Request 프로토콜 Encodable로 변경

--- MOUP/MOUP/Data/DTO/Request/AuthRequestDTOs.swift
+++ MOUP/MOUP/Data/DTO/Request/AuthRequestDTOs.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 /// 인증 관련(로그인, 회원가입 등) API 요청 시 필요한 모델들을 관리합니다.
-struct SignInRequestDTO {
+struct SignInRequestDTO: Encodable {
     let provider: String
     let idToken: String
 }
