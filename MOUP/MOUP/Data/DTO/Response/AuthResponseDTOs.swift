6e480b2ff711551ebc5616ce2567e4e8634960a7
SongKyuSeob <sks5075@naver.com>
2025-08-12 14:09:59 +0900

    chore: api response 변경에 따른 responseDTO 내부 변경

--- MOUP/MOUP/Data/DTO/Response/AuthResponseDTOs.swift
+++ MOUP/MOUP/Data/DTO/Response/AuthResponseDTOs.swift
@@ -10,4 +10,6 @@ import Foundation
 /// 인증 관련(로그인, 회원가입 등) API 호출 결과 관련 모델들을 관리합니다.
 struct SignInResponseDTO: Decodable {
     let userId: String? // 그 밖의 errorMessage 등 데이터들은 statusCode 기반을 넘어 추가 정보를 필요로 할 경우 추가
+    let role: String?
 }
+// TODO: - Service 구조 변경 (미리 statusCode 확인 후 파싱), DTO 내부 옵셔널 x
