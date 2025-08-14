2821b9f2b8d745a144d585653452e2c0f99ca912
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2025-08-14 18:28:50 +0900

    feat: #21 - 캘린더 근무 Entity 작성
    CalendarEvent

---
+++ MOUP/MOUP/Domain/Entities/CalendarEvent.swift
@@ -0,0 +1,28 @@
+//
+//  CalendarEvent.swift
+//  MOUP
+//
+//  Created by 서동환 on 8/14/25.
+//
+
+/// 캘린더 근무 Entity
+/// - `workDate`: 근무 날짜 `String`
+///   - 예시: "2025.08.14"
+/// - `startTime`: 출근 시간 `String`
+///   - 예시: "09:00"
+/// - `endTime`: 퇴근 시간 `String`
+///   - 예시: "18:00"
+/// - `restTime`: 휴게 시간 `Int`
+///   - 예시: 30
+/// - `memo`: 메모 `String`
+///   - 예시: "메모입니다."
+/// - `dailyIncome`: 일급 `Int`
+///   - 예시: 100300
+struct CalendarEvent {
+    let workDate: String
+    let startTime: String
+    let endTime: String
+    let restTime: Int
+    let memo: String
+    let dailyIncome: Int
+}
