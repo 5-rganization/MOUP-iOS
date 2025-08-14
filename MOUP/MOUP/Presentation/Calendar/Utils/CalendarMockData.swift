9f043477c58c2bc58129052eb2f534e704569b58
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2025-08-14 18:34:36 +0900

    feat: #21 - 캘린더 근무 목업 데이터 작성

---
+++ MOUP/MOUP/Presentation/Calendar/Utils/CalendarMockData.swift
@@ -0,0 +1,11 @@
+//
+//  CalendarMockData.swift
+//  MOUP
+//
+//  Created by 서동환 on 8/14/25.
+//
+
+enum CalendarMockData {
+    static let calendarEventListMock = [CalendarEvent(workDate: "2025.08.13", startTime: "09:00", endTime: "19:00", restTime: 60, memo: "테스트 메모", dailyIncome: 90270),
+                                        CalendarEvent(workDate: "2025.08.14", startTime: "12:00", endTime: "18:00", restTime: 30, memo: "테스트 메모", dailyIncome: 55165)]
+}
