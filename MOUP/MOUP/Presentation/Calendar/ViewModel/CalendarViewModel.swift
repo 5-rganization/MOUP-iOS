269ae0084b5b585678e1aa579e3cb630d82e5b8b
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2025-08-14 18:37:01 +0900

    feat: #21 - CalendarViewModel 일부 구현
    캘린더 근무 목업 데이터 방출

--- MOUP/MOUP/Presentation/Calendar/ViewModel/CalendarViewModel.swift
+++ MOUP/MOUP/Presentation/Calendar/ViewModel/CalendarViewModel.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+import RxRelay
+import RxSwift
+
+/// 캘린더 VM
+final class CalendarViewModel {
+    
+    // MARK: - Properties
+    private let disposeBag = DisposeBag()
+    
+    // MARK: - Input
+    struct Input {
+        let currMonth: Observable<(year: Int, month: Int)>
+        let selectedCalendarMode: Observable<CalendarMode>
+    }
+    
+    // MARK: - Output
+    struct Output {
+        let calendarModelList: Observable<[CalendarEvent]>
+    }
+    private let calendarModelListRelay = BehaviorRelay<[CalendarEvent]>(value: [])
+    
+    // MARK: - Input ➡️ Output Transform
+    func transform(input: Input) -> Output {
+        // TODO: 근무 이벤트 로딩
+        calendarModelListRelay.accept(CalendarMockData.calendarEventListMock)
+        return Output(calendarModelList: calendarModelListRelay.asObservable())
+    }
+}
