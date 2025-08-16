8b3bc4d25d30e99fa60e92608c81d52416d5d936
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2025-08-16 16:42:17 +0900

    feat: #20 - 필터 목업 데이터 작성

--- MOUP/MOUP/Presentation/Calendar/Utils/CalendarMockData.swift
+++ MOUP/MOUP/Presentation/Calendar/Utils/CalendarMockData.swift
@@ -6,8 +6,9 @@
 //
 
 enum CalendarMockData {
-    static let calendarEventListMock = (personal: [CalendarEvent(workDate: "2025.08.13", startTime: "09:00", endTime: "19:00", restTime: 60, memo: "개인 근무1", dailyIncome: 90270),
-                                                   CalendarEvent(workDate: "2025.08.14", startTime: "12:00", endTime: "18:00", restTime: 30, memo: "개인 근무2", dailyIncome: 55165)],
-                                        shared: [CalendarEvent(workDate: "2025.08.25", startTime: "09:00", endTime: "19:00", restTime: 60, memo: "공유 근무1", dailyIncome: 90270),
-                                                 CalendarEvent(workDate: "2025.08.26", startTime: "12:00", endTime: "18:00", restTime: 30, memo: "공유 근무2", dailyIncome: 55165)])
+    static let calendarEventListMock = (personal: [CalendarEvent(eventId: 123, workplaceId: 456, workplaceName: "세븐일레븐 동탄제일점", workerId: 789, workerName: "김알바", workDate: "2025.08.13", startTime: "09:00", endTime: "19:00", restTime: 60, memo: "개인 근무1", dailyIncome: 90270, colorLabel: "빨간색"),
+                                                   CalendarEvent(eventId: 124, workplaceId: 456, workplaceName: "세븐일레븐 동탄제일점", workerId: 789, workerName: "김알바", workDate: "2025.08.14", startTime: "12:00", endTime: "18:00", restTime: 30, memo: "개인 근무2", dailyIncome: 55165, colorLabel: "빨간색")],
+                                        shared: [CalendarEvent(eventId: 125, workplaceId: 456, workplaceName: "세븐일레븐 동탄제일점", workerId: 780, workerName: "이알바", workDate: "2025.08.25", startTime: "09:00", endTime: "19:00", restTime: 60, memo: "공유 근무1", dailyIncome: 90270, colorLabel: "노란색"),
+                                                 CalendarEvent(eventId: 126, workplaceId: 456, workplaceName: "세븐일레븐 동탄제일점", workerId: 780, workerName: "이알바", workDate: "2025.08.26", startTime: "12:00", endTime: "18:00", restTime: 30, memo: "공유 근무2", dailyIncome: 55165, colorLabel: "노란색")])
+    static let filterListMock = [FilterData(workplaceId: "456", workplaceName: "세븐일레븐 동탄제일점"), FilterData(workplaceId: "135", workplaceName: "쿠팡 동탄1센터")]
 }
