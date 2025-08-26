d4797ab645aac5246ec4a0516ea16e26933a30fd
SongKyuSeob <sks5075@naver.com>
2025-08-27 01:03:56 +0900

    feat: Bundle extension 구현

---
+++ MOUP/MOUP/Common/Utils/Extensions/Bundle+Extension.swift
@@ -0,0 +1,23 @@
+//
+//  Bundle+Extension.swift
+//  MOUP
+//
+//  Created by 송규섭 on 8/26/25.
+//
+
+import Foundation
+
+extension Bundle {
+    var googleClientID: String {
+        object(forInfoDictionaryKey: "GIDClientID") as? String ?? ""
+    }
+
+    var googleRedirectURI: String? {
+        if let urlTypes = infoDictionary?["CFBundleURLTypes"] as? [[String: Any]],
+           let urlSchemes = urlTypes.first?["CFBundleURLSchemes"] as? [String],
+           let reversedClientID = urlSchemes.first {
+            return "\(reversedClientID):/oauthredirect"
+        }
+        return nil
+    }
+}
