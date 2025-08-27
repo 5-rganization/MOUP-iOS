4e5d689cc0049eb2bea6dd70d267c8e6d2a77927
SongKyuSeob <sks5075@naver.com>
2025-08-28 01:50:56 +0900

    feat: 공통 컴포넌트인 BaseButton 내 타이틀의 폰트크기를 직접 조정할 수 있도록 변경

--- MOUP/MOUP/Presentation/Components/BaseButton.swift
+++ MOUP/MOUP/Presentation/Components/BaseButton.swift
@@ -11,9 +11,9 @@ import UIKit
 final class BaseButton: UIButton {
     
     // MARK: - Initializer
-    init(title: String = "적용하기", isSecondary: Bool = false) {
+    init(title: String = "적용하기", isSecondary: Bool = false, fontSize: CGFloat = 18) {
         super.init(frame: .zero)
-        configure(title: title, isSecondary: isSecondary)
+        configure(title: title, isSecondary: isSecondary, fontSize: fontSize)
     }
     
     @available(*, unavailable, message: "storyboard is not supported.")
@@ -22,32 +22,32 @@ final class BaseButton: UIButton {
     }
     
     // MARK: - Methods
-    func update(title: String, isSecondary: Bool = false) {
-        setConfiguration(title: title, isSecondary: isSecondary)
+    func update(title: String, isSecondary: Bool = false, fontSize: CGFloat = 18) {
+        setConfiguration(title: title, isSecondary: isSecondary, fontSize: fontSize)
     }
 }
 
 private extension BaseButton {
     // MARK: - configure
-    func configure(title: String, isSecondary: Bool) {
-        setStyles(title: title, isSecondary: isSecondary)
+    func configure(title: String, isSecondary: Bool, fontSize: CGFloat = 18) {
+        setStyles(title: title, isSecondary: isSecondary, fontSize: fontSize)
     }
     
     // MARK: - setStyles
-    func setStyles(title: String, isSecondary: Bool) {
-        setConfiguration(title: title, isSecondary: isSecondary)
-        
+    func setStyles(title: String, isSecondary: Bool, fontSize: CGFloat = 18) {
+        setConfiguration(title: title, isSecondary: isSecondary, fontSize: fontSize)
+
         self.clipsToBounds = true
         self.layer.cornerRadius = 12
     }
     
     // MARK: - setConfiguration
-    func setConfiguration(title: String, isSecondary: Bool) {
+    func setConfiguration(title: String, isSecondary: Bool, fontSize: CGFloat) {
         var config = UIButton.Configuration.filled()
         
-        let normalAttribute = AttributeContainer([.font: UIFont.buttonSemibold(18),
+        let normalAttribute = AttributeContainer([.font: UIFont.buttonSemibold(fontSize),
                                                   .foregroundColor: isSecondary ? UIColor.gray600 : UIColor.white])
-        let disableAttribute = AttributeContainer([.font: UIFont.buttonSemibold(18),
+        let disableAttribute = AttributeContainer([.font: UIFont.buttonSemibold(fontSize),
                                                    .foregroundColor: UIColor.gray500])
         let baseBackgroundColor: UIColor = isSecondary ? .gray200 : .accent
         config.attributedTitle = AttributedString(title, attributes: normalAttribute)
