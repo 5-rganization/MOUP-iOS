52776be0bed62f9d3cbbe8107ef7aca43cc3b7a5
SongKyuSeob <sks5075@naver.com>
2025-09-03 09:47:50 +0900

    feat: 로그인 소셜 플랫폼 구별 및 유저 역할 enum 정의

---
+++ MOUP/MOUP/Common/Enums/LoginProvider.swift
@@ -0,0 +1,13 @@
+//
+//  LoginProvider.swift
+//  MOUP
+//
+//  Created by 송규섭 on 9/3/25.
+//
+
+import Foundation
+
+enum LoginProvider: String {
+    case google = "LOGIN_GOOGLE"
+    case apple = "LOGIN_APPLE"
+}
---
+++ MOUP/MOUP/Common/Enums/UserRole.swift
@@ -0,0 +1,11 @@
+//
+//  UserRole.swift
+//  Routory
+//
+//  Created by 서동환 on 6/18/25.
+//
+
+enum UserRole: String, Decodable {
+    case worker = "ROLE_WORKER"
+    case owner = "ROLE_OWNER"
+}
