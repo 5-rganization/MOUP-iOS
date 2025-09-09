9b65c7cee7b896f0de215b129d5be4d0ab261b98
SongKyuSeob <sks5075@naver.com>
2025-09-09 19:01:12 +0900

    feat: EmployeeWorkSummary 추가 및 관련 엔티티 구조 변경

---
+++ MOUP/MOUP/Domain/Entities/EmployeeWorkSummary.swift
@@ -0,0 +1,14 @@
+//
+//  EmployeeWorkSummary.swift
+//  MOUP
+//
+//  Created by 송규섭 on 9/9/25.
+//
+
+import Foundation
+
+struct EmployeeWorkSummary {
+    let name: String
+    let workedDuration: String
+    let wage: Int
+}
--- MOUP/MOUP/Presentation/Home/ViewModel/HomeViewModel.swift
+++ MOUP/MOUP/Presentation/Home/ViewModel/HomeViewModel.swift
@@ -16,9 +16,25 @@ final class HomeViewModel {
         value: [HomeTableViewFirstSection(identity: 0, header: "", items: [])]
     ) // TODO: - nil을 허용할지 실제 백엔드 데이터 및 구조 확립 후 개선 필요
 
-    private let mockFirstSectionData = [HomeTableViewFirstSection(identity: 0, header: "", items: [
+    private let mockFirstSectionWorkerData = [HomeTableViewFirstSection(identity: 0, header: "", items: [
         HomeSectionItem.worker(WorkerWorkplaceCellInfo(workplace: WorkplaceData(id: "0", name: "세븐일레븐 성남시청점", category: "편의점", payType: "매월", payCalculation: "시급", salary: "10300", payDay: "3", nationalPension: true, healthInsurance: true, employmentInsurance: true, industrialAccidentInsurance: true, incomeTax: false, weeklyHolidayAllowance: true, nightShiftAllowance: true, colorLabel: "빨간색"), isOfficial: true)),
         HomeSectionItem.worker(WorkerWorkplaceCellInfo(workplace: WorkplaceData(id: "1", name: "이마트24 수진점", category: "편의점", payType: "매월", payCalculation: "시급", salary: "10300", payDay: "3", nationalPension: true, healthInsurance: true, employmentInsurance: true, industrialAccidentInsurance: true, incomeTax: false, weeklyHolidayAllowance: true, nightShiftAllowance: false, colorLabel: "빨간색"), isOfficial: false))
+    ])]
+
+    private let mockFirstWorkSummaries = [
+        EmployeeWorkSummary(name: "송알바", workedDuration: "8시간 05분", wage: 244300),
+        EmployeeWorkSummary(name: "송알바", workedDuration: "8시간 05분", wage: 244300),
+        EmployeeWorkSummary(name: "송알바", workedDuration: "8시간 05분", wage: 244300)
+    ]
+
+    private let mockSecondWorkSummaries = [
+        EmployeeWorkSummary(name: "송알바", workedDuration: "8시간 05분", wage: 244300),
+        EmployeeWorkSummary(name: "송알바", workedDuration: "8시간 05분", wage: 244300),
+    ]
+
+    private lazy var mockFirstSectionOwnerData = [HomeTableViewFirstSection(identity: 0, header: "", items: [
+        HomeSectionItem.owner(OwnerWorkplaceCellInfo(workplace: WorkplaceData(id: "0", name: "세븐일레븐 성남시청점", category: "편의점", payType: "매월", payCalculation: "시급", salary: "10300", payDay: "3", nationalPension: true, healthInsurance: true, employmentInsurance: true, industrialAccidentInsurance: true, incomeTax: false, weeklyHolidayAllowance: true, nightShiftAllowance: true, colorLabel: "빨간색"), isOfficial: true, workSummaries: mockFirstWorkSummaries)),
+        HomeSectionItem.owner(OwnerWorkplaceCellInfo(workplace: WorkplaceData(id: "1", name: "이마트24 수진점", category: "편의점", payType: "매월", payCalculation: "시급", salary: "10300", payDay: "3", nationalPension: true, healthInsurance: true, employmentInsurance: true, industrialAccidentInsurance: true, incomeTax: false, weeklyHolidayAllowance: true, nightShiftAllowance: false, colorLabel: "빨간색"), isOfficial: false, workSummaries: mockSecondWorkSummaries))
     ])]
 
     // MARK: - Input, Output
@@ -34,7 +50,7 @@ final class HomeViewModel {
     func transform(input: Input) -> Output {
         input.viewDidLoad.subscribe(onNext: { [weak self] in
             guard let self else { return }
-            mockFirstSectionDataRelay.accept(mockFirstSectionData)
+            mockFirstSectionDataRelay.accept(mockFirstSectionOwnerData)
         })
         .disposed(by: disposeBag)
 
