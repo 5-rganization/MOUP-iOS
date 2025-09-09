e790d558bcd941039dae3ea3a049c1e0b81a19b9
SongKyuSeob <sks5075@naver.com>
2025-09-09 15:15:14 +0900

    chore: 임시 isOfficial 프로퍼티 추가에 대한 목데이터 반영

--- MOUP/MOUP/Presentation/Home/ViewModel/HomeViewModel.swift
+++ MOUP/MOUP/Presentation/Home/ViewModel/HomeViewModel.swift
@@ -17,8 +17,8 @@ final class HomeViewModel {
     ) // TODO: - nil을 허용할지 실제 백엔드 데이터 및 구조 확립 후 개선 필요
 
     private let mockFirstSectionData = [HomeTableViewFirstSection(identity: 0, header: "", items: [
-        HomeSectionItem.worker(WorkerWorkplaceCellInfo(workplace: WorkplaceData(id: "0", name: "세븐일레븐 성남시청점", category: "편의점", payType: "매월", payCalculation: "시급", salary: "10300", payDay: "3", nationalPension: true, healthInsurance: true, employmentInsurance: true, industrialAccidentInsurance: true, incomeTax: false, weeklyHolidayAllowance: true, nightShiftAllowance: true, colorLabel: "빨간색"))),
-        HomeSectionItem.worker(WorkerWorkplaceCellInfo(workplace: WorkplaceData(id: "1", name: "이마트24 수진점", category: "편의점", payType: "매월", payCalculation: "시급", salary: "10300", payDay: "3", nationalPension: true, healthInsurance: true, employmentInsurance: true, industrialAccidentInsurance: true, incomeTax: false, weeklyHolidayAllowance: true, nightShiftAllowance: false, colorLabel: "빨간색")))
+        HomeSectionItem.worker(WorkerWorkplaceCellInfo(workplace: WorkplaceData(id: "0", name: "세븐일레븐 성남시청점", category: "편의점", payType: "매월", payCalculation: "시급", salary: "10300", payDay: "3", nationalPension: true, healthInsurance: true, employmentInsurance: true, industrialAccidentInsurance: true, incomeTax: false, weeklyHolidayAllowance: true, nightShiftAllowance: true, colorLabel: "빨간색"), isOfficial: true)),
+        HomeSectionItem.worker(WorkerWorkplaceCellInfo(workplace: WorkplaceData(id: "1", name: "이마트24 수진점", category: "편의점", payType: "매월", payCalculation: "시급", salary: "10300", payDay: "3", nationalPension: true, healthInsurance: true, employmentInsurance: true, industrialAccidentInsurance: true, incomeTax: false, weeklyHolidayAllowance: true, nightShiftAllowance: false, colorLabel: "빨간색"), isOfficial: false))
     ])]
 
     // MARK: - Input, Output
