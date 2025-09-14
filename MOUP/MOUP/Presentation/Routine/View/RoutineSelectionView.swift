df419268601e5fb4a189f56e4e20b77cef54a7b9
ksyq12 <ksyq12@daum.net>
2025-09-14 17:54:30 +0900

    feat: 루틴 선택 VC 생성
    Github Issue: #38

---
+++ MOUP/MOUP/Presentation/Routine/View/RoutineSelectionView.swift
@@ -0,0 +1,20 @@
+//
+//  RoutineSelectionView.swift
+//  MOUP
+//
+//  Created by shinyoungkim on 9/14/25.
+//
+
+import UIKit
+
+class RoutineSelectionView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ MOUP/MOUP/Presentation/Routine/ViewModel/RoutineSelectionViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  RoutineSelectionViewModel.swift
+//  MOUP
+//
+//  Created by shinyoungkim on 9/14/25.
+//
+
+import Foundation
