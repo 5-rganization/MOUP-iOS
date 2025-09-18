311bccd32d99bdd6e30430d62f780917daaa9db9
ksyq12 <ksyq12@daum.net>
2025-09-18 21:46:20 +0900

    fix: 할 일 추가 버튼 터치시 의도치않은 애니메이션 발생 문제 해결
    Github Issue: #38

--- MOUP/MOUP/Presentation/Routine/ViewModel/AddRoutineViewModel.swift
+++ MOUP/MOUP/Presentation/Routine/ViewModel/AddRoutineViewModel.swift
@@ -8,6 +8,14 @@
 import Foundation
 
 struct TodoItem: Hashable {
-    let id = UUID()
+    let id: UUID = UUID()
     var text: String
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+
+    static func ==(lhs: TodoItem, rhs: TodoItem) -> Bool {
+        return lhs.id == rhs.id
+    }
 }
