22ed058c8bbf1a3a42547b8b2ce1c32fbc7baa36
SongKyuSeob <sks5075@naver.com>
2025-09-26 19:06:36 +0900

    feat: Employee 기초 정보를 담는 엔티티 정의

---
+++ MOUP/MOUP/Domain/Entities/Employee.swift
@@ -0,0 +1,15 @@
+//
+//  Employee.swift
+//  MOUP
+//
+//  Created by 송규섭 on 9/26/25.
+//
+
+import Foundation
+
+/// 알바생에 대한 정보
+/// labelColor의 경우 String으로 받은 것을 UIColor로 labelColorString을 통해 변환 필요
+struct Employee {
+    let name: String
+    let labelColor: String
+}
