ed121162458a4f27a00f42eb430d91954fda291d
SongKyuSeob <sks5075@naver.com>
2025-10-20 01:55:53 +0900

    feat: #56 - 메모리 누수 방지 removeChildCoordinator 추가 및 초대 코드 입력 coordinator 구현

--- MOUP/MOUP/Coordinator/WorkplaceRegister/InviteCodeInputCoordinator.swift
+++ MOUP/MOUP/Coordinator/WorkplaceRegister/InviteCodeInputCoordinator.swift
@@ -5,4 +5,26 @@
 //  Created by 송규섭 on 10/18/25.
 //
 
-import Foundation
+import UIKit
+
+final class InviteCodeInputCoordinator: Coordinator {
+    weak var coordinator: WorkplaceRegisterSheetCoordinator?
+    var childCoordinators = [Coordinator]()
+    private let navigationController: UINavigationController
+    private let workplaceService: WorkplaceServiceProtocol
+    private let workplaceRepository: WorkplaceRepositoryProtocol
+    private let workplaceUseCase: WorkplaceUseCaseProtocol
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+        self.workplaceService = WorkplaceService()
+        self.workplaceRepository = WorkplaceRepository(workplaceService: workplaceService)
+        self.workplaceUseCase = WorkplaceUseCase(workplaceRepository: workplaceRepository)
+    }
+    
+    func start() {
+        let viewModel = InviteCodeInputViewModel(workplaceUseCase: workplaceUseCase)
+        let vc = InviteCodeInputViewController(viewModel: viewModel)
+        navigationController.pushViewController(vc, animated: true)
+    }
+}
