cd352328879d62290d3358f49751b61573de2d11
_GGDol <bohg6799@naver.com>
2025-10-30 20:57:48 +0900

    chore: #70 - DTO, Error, Entitie 생성 및 수정

---
+++ MOUP/MOUP/Data/DTO/Response/WorkplaceCreateResponseDTO.swift
@@ -0,0 +1,11 @@
+//
+//  WorkplaceCreateResponseDTO.swift
+//  MOUP
+//
+//  Created by 양원식 on 10/30/25.
+//
+
+// MARK: - Create Response DTO
+struct WorkplaceCreateResponseDTO: Codable {
+    let workplaceId: Int
+}
---
+++ MOUP/MOUP/Domain/Entities/WorkplaceCreate.swift
@@ -0,0 +1,10 @@
+//
+//  WorkplaceCreate.swift
+//  MOUP
+//
+//  Created by 양원식 on 10/30/25.
+//
+
+struct WorkplaceCreate {
+    let workplaceId: Int
+}
