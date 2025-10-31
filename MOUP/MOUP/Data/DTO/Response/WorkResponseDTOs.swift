3605f9a29730fb3c24078ebd07586ba92c7762ea
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2025-11-01 00:35:39 +0900

    fix: #75 - 중복되는 DTO 제거

--- MOUP/MOUP/Data/DTO/Response/WorkResponseDTOs.swift
+++ MOUP/MOUP/Data/DTO/Response/WorkResponseDTOs.swift
@@ -35,7 +35,7 @@ extension FailedWorkerInfoDTO {
 struct WorkDetailResponseDTO: Decodable {
     let workId: Int
     let workerSummaryInfo: WorkerSummaryResponseDTO
-    let workplaceSummaryInfo: WorkplaceSummaryResponseDTO
+    let workplaceSummaryInfo: WorkplaceSummaryInfoDTO
     let routineSummaryInfoList: [RoutineSummaryDTO]
     let workDate: String
     let startTime: Date
@@ -54,7 +54,7 @@ struct WorkDetailResponseDTO: Decodable {
 extension WorkDetailResponseDTO {
     func toDomain() -> WorkData {
         WorkData(id: workId,
-                 workplaceSummary: workplaceSummaryInfo.toDomain(),
+                 workplaceSummary: WorkplaceSummary(id: workplaceSummaryInfo.workplaceId, name: workplaceSummaryInfo.workplaceName, isShared: workplaceSummaryInfo.isShared),
                  workerSummary: workerSummaryInfo.toDomain(),
                  routineSummaryList: routineSummaryInfoList.map { $0.toDomain() },
                  workDate: workDate,
@@ -76,7 +76,7 @@ extension WorkDetailResponseDTO {
 struct WorkSummaryResponseDTO: Decodable {
     let workId: Int
     let workerSummaryInfo: WorkerSummaryResponseDTO
-    let workplaceSummaryInfo: WorkplaceSummaryResponseDTO
+    let workplaceSummaryInfo: WorkplaceSummaryInfoDTO
     let workDate: String
     let startTime: Date
     let endTime: Date?
@@ -92,7 +92,7 @@ struct WorkSummaryResponseDTO: Decodable {
 extension WorkSummaryResponseDTO {
     func toDomain() -> WorkSummary {
         WorkSummary(id: workId,
-                    workplaceSummary: workplaceSummaryInfo.toDomain(),
+                    workplaceSummary: WorkplaceSummary(id: workplaceSummaryInfo.workplaceId, name: workplaceSummaryInfo.workplaceName, isShared: workplaceSummaryInfo.isShared),
                     workerSummary: workerSummaryInfo.toDomain(),
                     workDate: workDate,
                     startTime: startTime,
