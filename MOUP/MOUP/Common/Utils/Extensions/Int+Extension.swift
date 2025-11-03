11c7b503fc945d185e990105ea4a8dbf39203769
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2025-11-04 04:04:09 +0900

    fix: #62 - 캘린더 근무 표시 UI의 근무 시간 소수점이 2개 표시되는 현상 수정

--- MOUP/MOUP/Common/Utils/Extensions/Int+Extension.swift
+++ MOUP/MOUP/Common/Utils/Extensions/Int+Extension.swift
@@ -15,13 +15,21 @@ extension Int {
         return "\(hours)시간 \(minutes)분"
     }
     
-    /// `Int` 타입으로 내려오는 TimeInterval 값을 "hh.m시간" 형태로 변환합니다.
-    /// - 예: "10.7시간"
+    /// `Int`값(분)을 시간으로 변환하며, 0.1시간(6분) 미만의 소수점은 버립니다.
+    /// - 예: 605분 (10시간 5분) ➡️ "10시간"
+    /// - 예: 606분 (10시간 6분) ➡️ "10.1시간"
+    /// - 예: 642분 (10시간 42분) ➡️ "10.7시간"
     var decimalTimeString: String {
         let hours = self / 60
         let minutes = self % 60
-        let minutesDecimal = Double(minutes) / 60.0
-        return "\(hours).\(minutesDecimal)시간"
+        
+        if (minutes < 6) {
+            return "\(hours)시간"
+        } else {
+            let minutesDecimal = Double(minutes) / 60.0
+            let totalHours = Double(hours) + minutesDecimal
+            return String(format: "%.1f시간", totalHours)
+        }
     }
     
     var formattedWithSeparator: String {
