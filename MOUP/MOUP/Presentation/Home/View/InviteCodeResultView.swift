905738bff55bff88fd02f4fb088a1c6735e2b712
SongKyuSeob <sks5075@naver.com>
2025-11-03 16:59:22 +0900

    chore: #56 - 확인용 알림 모달 animation false 처리

---
+++ MOUP/MOUP/Presentation/Home/View/InviteCodeResultView.swift
@@ -0,0 +1,20 @@
+//
+//  InviteCodeResultView.swift
+//  MOUP
+//
+//  Created by 송규섭 on 11/3/25.
+//
+
+import UIKit
+
+class InviteCodeResultView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ MOUP/MOUP/Presentation/Home/ViewModel/InviteCodeResultViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  InviteCodeResultViewModel.swift
+//  MOUP
+//
+//  Created by 송규섭 on 11/3/25.
+//
+
+import Foundation
