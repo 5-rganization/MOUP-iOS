9556291beb146bc9396e6ac2ec1d0cdb9f88564d
SongKyuSeob <sks5075@naver.com>
2025-11-06 01:07:43 +0900

    feat: #79 - 요청 api 수가 많아짐에 따라 관련 Error 케이스 추가

--- MOUP/MOUP/Data/Error/AttendanceError.swift
+++ MOUP/MOUP/Data/Error/AttendanceError.swift
@@ -8,19 +8,23 @@
 import Foundation
 
 enum AttendanceError: LocalizedError {
-    case invalidRequest // 400, Worker·Owner 근태 조회
-    case unauthorizedAccess // 403, Worker·Owner 근태 조회
-    case notFound // 404, Worker·Owner 근태 조회
+    case invalidRequest // 400, Worker·Owner 근태 조회 등
+    case unauthorizedAccess // 403, Worker·Owner 근태 조회 등
+    case notFound // 404, Worker·Owner 근태 조회 등
+    case invalidField // 422, FetchWorkers
+    case alreadyWorking // 409, startWork
 }
 
 extension AttendanceError {
     // 사용자 친화 에러 메시지
     var errorDescription: String? {
         switch self {
-        case .invalidRequest, .unauthorizedAccess:
+        case .invalidRequest, .unauthorizedAccess, .invalidField:
             "요청을 처리할 수 없습니다. 잠시 후 다시 시도해주세요."
         case .notFound:
             "요청하신 정보를 찾지 못했어요. 잠시 후 다시 시도해주세요."
+        case .alreadyWorking:
+            "해당 근무지에서 이미 근무 중이에요. 다시 확인해주세요."
         }
     }
 
@@ -33,6 +37,10 @@ extension AttendanceError {
             "근무 내역 조회 - 403"
         case .notFound:
             "근무 내역 조회 - 404"
+        case .invalidField:
+            "근무지 내 근무자 목록 조회 - 422"
+        case .alreadyWorking:
+            "근무 시작 - 409"
         }
     }
 }
