d0b85462244b49113ee0dd47c42dad3703380ea2
ksyq12 <ksyq12@daum.net>
2025-11-08 15:28:34 +0900

    refactor: DateFormatter에서 로케일 설정 제거
    Github Issue: #52

--- MOUP/MOUP/Data/Repositories/NotificationRepository.swift
+++ MOUP/MOUP/Data/Repositories/NotificationRepository.swift
@@ -60,7 +60,6 @@ final class NotificationRepository: NotificationRepositoryProtocol {
         for format in formats {
             let formatter = DateFormatter()
             formatter.dateFormat = format
-            formatter.locale = Locale(identifier: "en_US_POSIX")
             formatter.timeZone = TimeZone(identifier: "UTC")
             
             if let date = formatter.date(from: dateString) {
