8eade54d8473b9b4f20d18a8054666656b129040
_GGDol <bohg6799@naver.com>
2025-11-11 01:58:04 +0900

    feat: 근무 등록 근무지 선택 api 연동

--- MOUP/MOUP/Coordinator/WorkRegister/WorkRegisterCoordinator.swift
+++ MOUP/MOUP/Coordinator/WorkRegister/WorkRegisterCoordinator.swift
@@ -8,27 +8,56 @@
 import UIKit
 
 final class WorkRegisterCoordinator: WorkRegisterCoordinatorProtocol {
+    // MARK: - Properties
     var childCoordinators = [Coordinator]()
     private let navigationController: UINavigationController
-    private lazy var selectColorLabelViewModel = SelectColorLabelViewModel()
-    
-    func start() {
-        //let viewModel = WorkRegisterViewModel()
-        
-        let vc = WorkRegisterViewController(
-            coordinator: self,
-            workDatePickerViewModel: WorkDatePickerViewModel(),
-            clockInVM: WorkTimePickerViewModel(),
-            clockOutVM: WorkTimePickerViewModel()
+
+    // MARK: - Sub ViewModels
+    private lazy var selectedWorkplaceViewModel = SelectedWorkplaceViewModel(
+        useCase: WorkplaceUseCase(
+            workplaceRepository: WorkplaceRepository(
+                workplaceService: WorkplaceService()
+            )
         )
-        
-        navigationController.pushViewController(vc, animated: false)
-    }
-    
+    )
+    private lazy var workDatePickerViewModel = WorkDatePickerViewModel()
+    private lazy var clockInViewModel = WorkTimePickerViewModel()
+    private lazy var clockOutViewModel = WorkTimePickerViewModel()
+    private lazy var breakPickerViewModel = WorkBreakPickerViewModel()
+    private lazy var selectColorLabelViewModel = SelectColorLabelViewModel()
+
+    // MARK: - Init
     init(navigationController: UINavigationController) {
         self.navigationController = navigationController
     }
-    
+
+    // MARK: - Start
+    func start() {
+        
+        let viewModel = WorkRegisterViewModel(
+            selectedWorkplaceVM: selectedWorkplaceViewModel,
+            datePickerVM: workDatePickerViewModel,
+            clockInVM: clockInViewModel,
+            clockOutVM: clockOutViewModel,
+            breakPickerVM: breakPickerViewModel,
+            workUseCase: WorkUseCase(workRepository: WorkRepository(workService: WorkService()))
+        )
+
+        let vc = WorkRegisterViewController(
+            viewModel: viewModel,
+            coordinator: self
+        )
+
+        vc.hidesBottomBarWhenPushed = true
+        navigationController.pushViewController(vc, animated: false)
+    }
+
+    // MARK: - Navigation
+    func showSelectWorkplace() {
+        let vc = SelectedWorkplaceViewController(viewModel: selectedWorkplaceViewModel)
+        navigationController.pushViewController(vc, animated: true)
+    }
+
     func showSelectColorLabel() {
         let vc = SelectColorLabelViewController(viewModel: selectColorLabelViewModel)
         navigationController.pushViewController(vc, animated: true)
