ff80f191f6bf5d42616e8ac91541d39b421676a9
ksyq12 <ksyq12@daum.net>
2025-11-12 22:21:19 +0900

    refactor: EditRoutineView에서 tag 패턴을 Rx 바인딩으로 개선
    Github Issue: #98

--- MOUP/MOUP/Presentation/Routine/View/EditRoutineView.swift
+++ MOUP/MOUP/Presentation/Routine/View/EditRoutineView.swift
@@ -23,7 +23,16 @@ final class EditRoutineView: UIView {
             assertionFailure("TodoCell 생성 실패 - 셀이 등록되지 않았거나 타입이 잘못됨")
             return UITableViewCell()
         }
+        cell.disposeBag = DisposeBag()
+        
         cell.textField.text = item.content
+        
+        cell.textField.rx.text.orEmpty
+            .skip(1)
+            .map { text in (index: indexPath.row, text: text) }
+            .bind(to: self.itemTextChangeRelay)
+            .disposed(by: cell.disposeBag)
+        
         return cell
     }
     fileprivate let itemTextChangeRelay = PublishRelay<(index: Int, text: String)>()
@@ -250,17 +259,6 @@ private extension EditRoutineView {
 }
 
 extension EditRoutineView: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        guard let cell = cell as? TodoCell else { return }
-        cell.textField.tag = indexPath.row
-        cell.textField.removeTarget(nil, action: nil, for: .editingChanged)
-        cell.textField.addTarget(self, action: #selector(textChanged(_:)), for: .editingChanged)
-    }
-    
-    @objc private func textChanged(_ tf: UITextField) {
-        itemTextChangeRelay.accept((index: tf.tag, text: tf.text ?? ""))
-    }
-    
     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         let deleteAction = UIContextualAction(style: .destructive, title: "삭제") { [weak self] _, _, completion in
             self?.itemDeleteRelay.accept(indexPath.row)
@@ -281,7 +279,6 @@ extension EditRoutineView: UITableViewDelegate {
 }
 
 extension EditRoutineView: UITableViewDragDelegate {
-
     func tableView(_ tableView: UITableView,
                    itemsForBeginning session: UIDragSession,
                    at indexPath: IndexPath) -> [UIDragItem] {
