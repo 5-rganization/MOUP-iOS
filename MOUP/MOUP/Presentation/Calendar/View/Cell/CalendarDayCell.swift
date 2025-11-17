aea0bb354d58fc979148225b85e2f6e417e5ea7f
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2025-11-17 17:55:26 +0900

    refactor: #104 - systemLayoutSizeFitting을 DispatchQueue.main.async로 변경

--- MOUP/MOUP/Presentation/Calendar/View/Cell/CalendarDayCell.swift
+++ MOUP/MOUP/Presentation/Calendar/View/Cell/CalendarDayCell.swift
@@ -54,29 +54,21 @@ final class CalendarDayCell: JTACDayCell {
     override func layoutSubviews() {
         super.layoutSubviews()
         
-        // workContainerVStackView의 minY 계산
-        let workContainerMinY = dayLabel.frame.maxY + 4
-        
-        // workContainerVStackView의 너비 계산
-        let targetWidth = self.contentView.bounds.width - 4
-        // systemLayoutSizeFitting에 전달할 목표 크기 계산 - 너비는 targetWidth, 높이는 시스템이 계산
-        let targetSize = CGSize(width: targetWidth, height: UIView.layoutFittingCompressedSize.height)
-        // targetSize를 이용하여 workContainerVStackView의 잠재적인 최대 높이 계산
-        let requiredHeight = workContainerVStackView.systemLayoutSizeFitting(targetSize,
-                                                                             withHorizontalFittingPriority: .required,
-                                                                             verticalFittingPriority: .fittingSizeLevel).height
-        // workContainerVStackView의 잠재적인 maxY 계산
-        let potentialMaxY = workContainerMinY + requiredHeight
-        
-        // workContainerVStackView의 최대 maxY 계산(여백 4 포함)
-        let possibleMaxY = self.contentView.bounds.height - 4
-        
-        // 근무 컨테이너 UI가 캘린더 셀을 벗어나거나 여백이 4 미만일 때
-        if potentialMaxY >= possibleMaxY {
-            let possibleMaxHeight = possibleMaxY - workContainerMinY
-            let reducedCount = Int(possibleMaxHeight / (WorkRowSize.baseComponentHeight + workContainerVStackView.spacing))
-            // 근무 UI 크기 줄임
-            workContainerVStackView.reduceHeight(displayCount: reducedCount)
+        DispatchQueue.main.async {
+            let workContainerMaxY = self.workContainerVStackView.frame.maxY
+            
+            // 근무 컨테이너 UI의 maxY가 가능한 최댓값
+            let possibleMaxY = self.contentView.bounds.height - 4  // 하단 여백 4 포함
+            
+            // 근무 컨테이너 UI와 캘린더 셀 사이 여백이 4 미만일 때
+            if workContainerMaxY >= possibleMaxY {
+                let workContainerMinY = self.workContainerVStackView.frame.minY
+                let possibleMaxHeight = possibleMaxY - workContainerMinY
+                
+                let reducedCount = Int(possibleMaxHeight / (WorkRowSize.baseComponentHeight + self.workContainerVStackView.spacing))
+                
+                self.workContainerVStackView.reduceHeight(displayCount: reducedCount)
+            }
         }
     }
     
@@ -111,6 +103,8 @@ final class CalendarDayCell: JTACDayCell {
         case .shared:
             workContainerVStackView.updateSharedModeWorkRows(workList: workList, displayCount: displayCount)
         }
+        
+        setNeedsLayout()
     }
 }
 
